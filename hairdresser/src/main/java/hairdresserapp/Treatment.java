ae12a749b7dfe9037f8e9aab3d7768a8d27bcbcc
Anettkva <anettkva@stud.ntnu.no>
2024-09-16 13:24:25 +0200

    Implemented the Treatment class with getters and setters

--- hairdresser/src/main/java/hairdresserapp/Treatment.java
+++ hairdresser/src/main/java/hairdresserapp/Treatment.java
@@ -1,5 +1,36 @@
 package main.java.hairdresserapp;
 
 public class Treatment {
-    
+    private int price;
+    private int durationMinutes;
+
+    public Treatment(int price, int durationMinutes) {
+        if (price < 0 || durationMinutes < 0 || durationMinutes > 420) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+        this.durationMinutes = durationMinutes;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price can't be negative");
+        }
+        this.price = price;
+    }
+
+    public int getduration() {
+        return durationMinutes;
+    }
+
+    public void setduration(int durationMinutes) {
+        if (durationMinutes < 0 || durationMinutes > 420) {
+            throw new IllegalArgumentException("duration must be between 0 and 420 minutes long");
+        }
+        this.durationMinutes = durationMinutes;
+    }
 }
