7d55757685123c68c60a9f65a7ccda64dae41421
Klara <kmfiguei@ntnu.no>
2024-09-17 10:43:38 +0200

    testing PriceCalculator-method

---
+++ hairdresserapp/src/test/java/app/PriceCalculator.java
@@ -0,0 +1,29 @@
+package app;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+
+
+public class PriceCalculator {
+
+
+public class PriceCalculatorTest {
+    @Test
+    public void calculateTotalPrice(){
+        Treatment haircut = new Treatment("Haircut",500, 60);
+        Treatment coloring = new Treatment("Hair Coloring",700,90);
+        Treatment styling = new Treatment("Styling", 400,90);
+
+        List<Treatment> treatments = Arrays.asList(haircut,coloring,styling);
+
+        PriceCalculator priceCalculator = new PriceCalculator();
+
+        double totalPrice = priceCalculator.calculateTotalPrice(treatments);
+
+        assertEquals(1600, totalPrice, "total prie should be 1000");
+    }
+    
+}
+
+    
+
