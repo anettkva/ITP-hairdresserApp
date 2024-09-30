5b6e971f4c4fc14ed80c68c1b1295dab28c5cab8
CamillaBolkan <camilsbo@stud.ntnu.no>
2024-09-30 13:05:29 +0200

    start på endringer av mappestruktur, må fikse module-info og filhåndtering fortsatt

---
+++ core/src/main/java/core/Treatment.java
@@ -0,0 +1,51 @@
+package core;
+
+public class Treatment {
+    private int price;
+    private int durationMinutes;
+    private String name;
+
+    public Treatment(String name, int price, int durationMinutes) {
+        if (price < 0 || durationMinutes < 0 || durationMinutes > 420) {
+            throw new IllegalArgumentException();
+        }
+        this.price = price;
+        this.durationMinutes = durationMinutes;
+        this.name = name;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price < 0) {
+            throw new IllegalArgumentException("Price can't be negative");
+        }
+        this.price = price;
+    }
+
+    public int getduration() {
+        return durationMinutes;
+    }
+
+    public void setduration(int durationMinutes) {
+        if (durationMinutes < 0 || durationMinutes > 420) {
+            throw new IllegalArgumentException("duration must be between 0 and 420 minutes long");
+        }
+        this.durationMinutes = durationMinutes;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+
+    
+
+}
