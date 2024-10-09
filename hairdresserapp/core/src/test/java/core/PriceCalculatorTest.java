90bfcceca5485b37d9df1c4541c4ace5490b8b88
Klara <kmfiguei@ntnu.no>
2024-10-09 10:04:51 +0200

    legger til en test for Pricecalculator-klassen

--- hairdresserapp/core/src/test/java/core/PriceCalculatorTest.java
+++ hairdresserapp/core/src/test/java/core/PriceCalculatorTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 public class PriceCalculatorTest {
     @Test
-    public void calculateTotalPrice(){
+    public void calculateTotalPriceTest(){
         Treatment haircut = new Treatment("Haircut",500, 60);
         Treatment coloring = new Treatment("Hair Coloring",700,90);
         Treatment styling = new Treatment("Styling", 400,90);
@@ -23,6 +23,15 @@ public class PriceCalculatorTest {
         double totalPrice = priceCalculator.CalculateTotalPrice(treatments);
 
         assertEquals(1600, totalPrice, "total pries should be 1000");
+    }
+
+
+    @Test
+    public void caculateTotalPrice_EmptyListTest(){
+        List<Treatment> emptyList = Arrays.asList();
+        PriceCalculator priceCalculator = new PriceCalculator();
+        double totalPrice = priceCalculator.CalculateTotalPrice(emptyList);
+        assertEquals(0.0, totalPrice, "totalprisen burde være 0.0 for en tom liste");
     }
     
 }
