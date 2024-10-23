451855ca018635222e8acc7b85ccb88f529da001
Anettkva <anettkva@stud.ntnu.no>
2024-10-23 11:28:10 +0200

    feat: implementert kode for TreatmentService #63
    Co-authored-by: Klara Maria Stokkvold <kmfiguei@ntnu.stud.no>

--- hairdresserapp/backend/src/main/java/backend/TreatmentService.java
+++ hairdresserapp/backend/src/main/java/backend/TreatmentService.java
@@ -11,5 +11,32 @@ public class TreatmentService {
     @Autowired
     private TreatmentRepository TreatmentRepository;
 
-    
+
+    public List<Treatment> getAllTreatments() {
+        return treatmentRepository.findAll();
+    }
+
+    public Treatment getTreatmentById(Long id) {
+        return treatmentRepository.findById(id).orElse(null);
+    }
+
+    public Treatment createTreatment(Treatment treatment) {
+        return treatmentRepository.save(treatment);
+    }
+
+    public Treatment updateTreatment(Long id, Treatment treatmentDetails) {
+        return treatmentRepository.findById(id).map(treatment -> {
+            treatment.setName(treatmentDetails.getName());
+            treatment.setPrice(treatmentDetails.getPrice());
+
+            return treatmentRepository.save(treatment);
+        }).orElse(null);
+    }
+
+    public boolean deleteTreatment(Long id) {
+        return treatmentRepository.findById(id).map(treatment -> {
+            treatmentRepository.delete(treatment);
+            return true;
+        }).orElse(false);
+    }
 }
